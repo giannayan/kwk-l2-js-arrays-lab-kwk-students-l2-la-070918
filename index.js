fa4b26f950786903bfd135bc7f9d9bb39e290b3f
giannayan <giannayan1@gmail.com>
2018-07-13 22:50:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const drivers = ['Milo', 'Otis', 'Garfield']
 function destructivelyAppendDriver(name) {
 return drivers.push(name)
 }
+
+
+function destructivelyAppendDriver(name) {
+return drivers.push(name)
+}
